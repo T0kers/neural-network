9a5072941a0e12b5b9bc2df9bfa6c41deaa7f3bc
Toke <tokerskovhuus@gmail.com>
2026-06-24 15:09:09 +0200

    Network can now do predictions on handdrawn digits

--- main.py
+++ main.py
@@ -33,23 +33,42 @@
 # Retrieved 2026-06-23, License - CC BY-SA 3.0
 
 import tkinter as tk
+import tkinter.font as tkFont
 from PIL import Image, ImageTk
+
 import numpy as np
 
 import load_mnist
+import nn
 
 dim = 28
 canvas_size = 600
 cell_size = canvas_size // dim
 
 training_inputs = load_mnist.load_data("mnist_data/t10k-images.idx3-ubyte")
-img_data = training_inputs[1] / 255
-#img_data = np.zeros((dim, dim))
+training_labels = load_mnist.load_labels("mnist_data/t10k-labels.idx1-ubyte")
+
+image_index = 0
+img_data = np.zeros((dim, dim))
+
+def inc_image(event=None):
+    global image_index, img_data
+    img_data = training_inputs[image_index] / 255
+    l_target.config(text=training_labels[image_index])
+    image_index += 1
+    render()
 
 root = tk.Tk()
 
-c = tk.Canvas(root, height=600, width=1000, bg='gray')
-c.pack(fill=tk.BOTH, expand=True)
+font = tkFont.Font(family="Arial", size=25)
+
+l_target = tk.Label(root, height=1, width=10, font=font)
+canvas = tk.Canvas(root, height=600, width=600)
+l_prediction = tk.Label(root, height=12, width=10, font=font, anchor="w", justify="left", padx=50)
+
+l_target.grid(row=0, column=0)
+canvas.grid(row=1, column=0)
+l_prediction.grid(row=1, column=1)
 
 img_id = None  # canvas image handle
 tk_img = None   # keep reference to avoid garbage collection
@@ -68,33 +87,9 @@ def render():
 
     # Draw on canvas (replace previous image)
     if img_id is None:
-        img_id = c.create_image(0, 0, anchor="nw", image=tk_img)
+        img_id = canvas.create_image(0, 0, anchor="nw", image=tk_img)
     else:
-        c.itemconfig(img_id, image=tk_img)
-
-def create_grid(event=None):
-    c.delete('grid_line') # Will only remove the grid_line
-
-    # Creates all vertical lines at intevals of 100
-    for i in range(0, dim + 1):
-        x = i / dim * canvas_size
-        y = i / dim * canvas_size
-        c.create_line([(x, 0), (x, canvas_size)], tag='grid_line')
-        c.create_line([(0, y), (canvas_size, y)], tag='grid_line')
-
-
-def paint(event):
-    x = event.x
-    y = event.y
-
-    # Convert pixel position -> grid index
-    j = min(dim - 1, max(0, x // cell_size))
-    i = min(dim - 1, max(0, y // cell_size))
-
-    # Set pixel (black ink)
-    img_data[i, j] = 1.0
-
-    render()
+        canvas.itemconfig(img_id, image=tk_img)
 
 
 def lin_paint_fallof(x):
@@ -105,7 +100,7 @@ def gauss_paint_fallof(x):
     return np.exp(-0.45 * (x / sigma)**2) / 2
 
 
-def paint2(event):
+def paint(event):
     global img_data
 
     x = event.x
@@ -120,19 +115,40 @@ def paint2(event):
     img_data = np.minimum(1, img_data)
 
     render()
+    predict()
 
 def clear(event=None):
     global img_data
     img_data.fill(0)
+    l_target.config(text="")
     render()
 
-c.bind("<B1-Motion>", paint2)
-c.bind("<Button-1>", paint2)
+network = nn.Sequential([
+    nn.Linear(28 * 28, 100),
+    nn.Sigmoid(100),
+    nn.Linear(100, 10),
+    nn.Softmax(10, True),
+])
 
-# c.bind('<Configure>', create_grid)
-# c.bind('<Configure>', create_grid)
+network.load("testnet.npz")
+
+def predict():
+    global network, img_data
+    prediction = np.stack((np.arange(10), network.forward(img_data.ravel())), axis=1)
+
+    text = ""
+    for i, p in np.flip(prediction[prediction[:, 1].argsort()], axis=0):
+        text += f"{int(i)}: {int(100 * p)}%\n"
+    l_prediction.config(text=text)
+    canvas.after(2000, predict)
+
+canvas.bind("<B1-Motion>", paint)
+canvas.bind("<Button-1>", paint)
+
 root.bind("c", clear)
+root.bind("i", inc_image)
 
-render()
+inc_image()
+predict()
 
 root.mainloop()
